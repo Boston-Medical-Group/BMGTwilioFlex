df749871dc67be5b9e4a2efd1faba734792eaa9e
Jean Rumeau <rumeau@gmail.com>
2023-12-19 10:47:13 -0300

    agrega dev

---
+++ Functions/assets/callerIdPools/ES.private.js
@@ -0,0 +1,20 @@
+exports.default = {
+    // TaskRouter -> Workspace -> TaskQueues
+
+    // Front
+    defaultQueue: 'WQc6c8945587376b86f452147a38062acb',
+
+    // Front
+    'WQc6c8945587376b86f452147a38062acb': [
+        "+13516665240",
+    ],
+
+    // Manual
+    'WQc52442201ec36719809754e401a944b5': [
+        "+13516665240",
+    ],
+
+    // PAlta
+
+    // PBaja
+}
