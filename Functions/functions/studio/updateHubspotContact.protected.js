63d7f9a382e805e272ea103ae9a1696182295e21
Jean Rumeau <rumeau@gmail.com>
2023-12-27 19:49:38 -0300

    guarda datos twilio flex

--- Functions/functions/studio/updateHubspotContact.protected.js
+++ Functions/functions/studio/updateHubspotContact.protected.js
@@ -4,21 +4,6 @@ const axios = require("axios");
 * If the CRM has a duplicate number, the function returns the first record (usually the oldest)
 */
 
-const countryMap = {
-  ecu: 'EC',
-  can: 'CA',
-  mex: 'MX',
-  col: 'CO',
-  arg: 'AR',
-  esp: 'ES',
-  spa: 'ES',
-  per: 'PE',
-  ale: 'DE',
-  deu: 'DE',
-  bra: 'BR',
-  dev: 'ES',
-};
-
 exports.handler = async function (context, event, callback) {
   let result = {
     crmid: '',
@@ -28,33 +13,26 @@ exports.handler = async function (context, event, callback) {
     lifecyclestage: ''
   };
   let from = event.from;
-  let to = event.to;
+  let to   = event.to;
 
   //if the string from contains a whatsapp prefix we need to remove it
   from = from.replace('whatsapp:', '');
-  to = to.replace('whatsapp:', '');
+  to   = to.replace('whatsapp:', '');
   axios({
-    url: `https://api.hubapi.com/crm/v3/objects/contacts`,
-    method: "POST",
+    url: `https://api.hubapi.com/crm/v3/objects/contacts/${event.crmid}`,
+    method: "PATCH",
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${context.HUBSPOT_TOKEN}`
     },
     data: {
       properties: {
-        firstname: 'Anonymous',
-        lastname: 'Contact',
-        phone: from,
-        hs_lead_status: 'NEW',
-        tipo_de_lead: 'Llamada',
-        country: countryMap[context.COUNTRY],
         tf_inbound_ddi: to.replace(/\s/g, ""),
         tf_inbound_date: Math.round((new Date()).getTime() / 1000),
       }
     }
   }).then(function (response) {
     let contact = response.data;
-    console.log(contact, response.data)
     //the result object stores the data you need from hubspot. In this example we're returning the CRM ID, first name and last name only.
     result.crmid = `${contact.id}`; //required for screenpop
     result.firstname = `${contact.properties.firstname}`;
