0d33c16cf82210c505847509d2b3c59421749bc7
Jean Rumeau <rumeau@gmail.com>
2024-01-24 22:48:09 -0300

    test react polling

--- Flex/flex-quick-schedule-button/src/components/CalendarButton/CalendarButton.tsx
+++ Flex/flex-quick-schedule-button/src/components/CalendarButton/CalendarButton.tsx
@@ -4,6 +4,7 @@ import { CalendarIcon } from '@twilio-paste/icons/esm/CalendarIcon'
 import { Spinner } from '@twilio-paste/core/spinner'
 import { Flex } from '@twilio/flex-ui/src/FlexGlobal'
 import useApi from '../../hooks/useApi'
+import { useQuery } from '@tanstack/react-query'
 
 type MyProps = {
   manager: Flex.Manager
@@ -13,51 +14,43 @@ type MyProps = {
 export const CalendarButton = ({ manager, task }: MyProps) => {
   const { getCalendarUrl } = useApi({ token: manager.store.getState().flex.session.ssoTokenPayload.token });
 
-  const [isLoading, setIsLoading] = useState(false)
   const [pollCounter, setPollCounter] = useState(0)
   const [calendarUrl, setCalendarUrl] = useState('')
+  const timerIdRef = useRef<string | number | undefined | NodeJS.Timeout>();
   const [isPollingEnabled, setIsPollingEnabled] = useState(true);
   const [runPoll, setRunPoll] = useState(false)
-  const [timerId, setTimerId] = useState<NodeJS.Timeout | undefined>()
 
-  // Escribe código para React que haga poll a un servicio ajax cada 5 segundos hasta recibir un resultado esperado o hasta que lo haya intentado 4 veces.
-  useEffect(() => {
-    setIsLoading(true)
-    const pollingCallback = async () => {
-      const response = await getCalendarUrl(task)
-      if (response.calendarUrl !== null && response.calendarUrl !== '') {
-        setIsLoading(false)
-        setCalendarUrl(response.calendarUrl)
-        clearInterval(timerId);
-      } else {
-        setPollCounter((prevCounter) => prevCounter + 1);
+  const { isLoading, isError, data, error } = useQuery({
+    //@ts-ignore
+    queryKey: ['calendarUrl'],
+    queryFn: (new Promise(async (resolve, reject) => {
+      await getCalendarUrl(task)
+        .then(({ calendarUrl }) => {
+          if (calendarUrl !== null && calendarUrl !== '') {
+            setCalendarUrl(calendarUrl)
+            resolve(calendarUrl)
+          } else {
+            reject('error')
+          }
+        }).catch((err) => {
+          reject(error)
+        })
+    }))
+  })
 
-        if (pollCounter >= 5) {
-          setIsLoading(false)
-          clearInterval(timerId);
-        }
-      }
-    };
+  
 
-    setTimerId(setInterval(pollingCallback, 5000))
+  const sendCalendarHandler = useCallback((data) => {
+    window.open(data, '_blank');
+  }, [])
 
-    return () => {
-      setIsLoading(false)
-      clearInterval(timerId);
-    };
-  }, [timerId]);
-
-  const sendCalendarHandler = useCallback(() => {
-    window.open(calendarUrl, '_blank');
-  }, [calendarUrl])
-
-  if (calendarUrl === '') {
+  if (calendarUrl === '' || isLoading) {
     return null
   }
 
   return (
     <>
-      <Button variant="primary" onClick={sendCalendarHandler} size="icon_small" style={{paddingTop: '0.32rem', paddingBottom: '0.32rem', minWidth: 'auto', marginRight: '0.25rem'}}>
+      <Button variant="primary" onClick={sendCalendarHandler(data)} size="icon_small" style={{paddingTop: '0.32rem', paddingBottom: '0.32rem', minWidth: 'auto', marginRight: '0.25rem'}}>
         {isLoading ? (
           <Spinner size='sizeIcon10' decorative={false} title='Loading' />
         ) : (
