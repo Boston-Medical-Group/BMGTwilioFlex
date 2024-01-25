112609a9efd2bb90f28217774db37af48afa7c7f
Jean Rumeau <rumeau@gmail.com>
2024-01-24 23:15:12 -0300

    rollback

--- Flex/flex-quick-schedule-button/src/components/CalendarButton/CalendarButton.tsx
+++ Flex/flex-quick-schedule-button/src/components/CalendarButton/CalendarButton.tsx
@@ -4,7 +4,6 @@ import { CalendarIcon } from '@twilio-paste/icons/esm/CalendarIcon'
 import { Spinner } from '@twilio-paste/core/spinner'
 import { Flex } from '@twilio/flex-ui/src/FlexGlobal'
 import useApi from '../../hooks/useApi'
-import { useQuery } from '@tanstack/react-query'
 
 type MyProps = {
   manager: Flex.Manager
@@ -14,43 +13,72 @@ type MyProps = {
 export const CalendarButton = ({ manager, task }: MyProps) => {
   const { getCalendarUrl } = useApi({ token: manager.store.getState().flex.session.ssoTokenPayload.token });
 
+  const [isLoading, setIsLoading] = useState(false)
   const [pollCounter, setPollCounter] = useState(0)
   const [calendarUrl, setCalendarUrl] = useState('')
   const timerIdRef = useRef<string | number | undefined | NodeJS.Timeout>();
   const [isPollingEnabled, setIsPollingEnabled] = useState(true);
   const [runPoll, setRunPoll] = useState(false)
 
-  const { isLoading, isError, data, error } = useQuery({
-    //@ts-ignore
-    queryKey: ['calendarUrl'],
-    queryFn: (new Promise(async (resolve, reject) => {
-      await getCalendarUrl(task)
-        .then(({ calendarUrl }) => {
-          if (calendarUrl !== null && calendarUrl !== '') {
-            setCalendarUrl(calendarUrl)
-            resolve(calendarUrl)
-          } else {
-            reject('error')
-          }
-        }).catch((err) => {
-          reject(error)
-        })
-    }))
-  })
-
-  
-
-  const sendCalendarHandler = useCallback((data) => {
-    window.open(data, '_blank');
+  useEffect(() => {
+    setRunPoll(true)
   }, [])
 
-  if (calendarUrl === '' || isLoading) {
+  useEffect(() => {
+    setIsLoading(true)
+    
+    const pollingCallback = async () => {
+      await getCalendarUrl(task)
+        .then(({ calendarUrl }: { calendarUrl: string }) => {
+          if (calendarUrl !== null && calendarUrl !== '') {
+            //setPollCounter(0)
+            setCalendarUrl(calendarUrl)
+            setRunPoll(false)
+          }
+        }).catch(err => {
+          //setCalendarUrl('')
+        }).finally(() => {
+          setPollCounter(pollCounter + 1)
+          console.log(pollCounter)
+          if (pollCounter > 5) {
+            setPollCounter(0)
+            setRunPoll(false)
+            setIsPollingEnabled(false)
+          }
+        })
+    };
+
+    const startPolling = () => {
+      timerIdRef.current = setInterval(pollingCallback, 5000);
+    };
+
+    const stopPolling = () => {
+      setIsLoading(false)
+      clearInterval(timerIdRef.current);
+    };
+
+    if (runPoll && (calendarUrl === null || calendarUrl === '')) {
+      startPolling();
+    } else {
+      stopPolling();
+    }
+
+    return () => {
+      stopPolling();
+    };
+  }, [runPoll, isPollingEnabled])
+
+  const sendCalendarHandler = useCallback(() => {
+    window.open(calendarUrl, '_blank');
+  }, [calendarUrl])
+
+  if (calendarUrl === '') {
     return null
   }
 
   return (
     <>
-      <Button variant="primary" onClick={sendCalendarHandler(data)} size="icon_small" style={{paddingTop: '0.32rem', paddingBottom: '0.32rem', minWidth: 'auto', marginRight: '0.25rem'}}>
+      <Button variant="primary" onClick={sendCalendarHandler} size="icon_small" style={{paddingTop: '0.32rem', paddingBottom: '0.32rem', minWidth: 'auto', marginRight: '0.25rem'}}>
         {isLoading ? (
           <Spinner size='sizeIcon10' decorative={false} title='Loading' />
         ) : (
