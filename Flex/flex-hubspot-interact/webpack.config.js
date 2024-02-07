8b0e302de74297d70e5e015752dd95228806b980
Jean Rumeau <rumeau@gmail.com>
2024-02-07 12:29:04 -0300

    build

--- Flex/flex-hubspot-interact/webpack.config.js
+++ Flex/flex-hubspot-interact/webpack.config.js
@@ -1,12 +1,8 @@
-const TerserPlugin = require('terser-webpack-plugin');
-
 module.exports = (config, { isProd, isDev, isTest }) => {
   /**
    * Customize the webpack by modifying the config object.
    * Consult https://webpack.js.org/configuration for more information
    */
 
-  config.optimization.minimizer = [new TerserPlugin()]
-  
   return config;
 }
