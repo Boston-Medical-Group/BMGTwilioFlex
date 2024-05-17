f0cb0d6660724d9c1a1dbde7e807e526357cebfd
Jean Rumeau <rumeau@gmail.com>
2024-05-17 08:10:56 -0400

    agrega async a function

--- Functions/functions/studio/loadDialingPermissions.protected.js
+++ Functions/functions/studio/loadDialingPermissions.protected.js
@@ -1,19 +1,19 @@
 "use strict";
 const TokenValidator = require('twilio-flex-token-validator').functionValidator;
 
-exports.handler = TokenValidator(function (context, event, callback) {
+exports.handler = TokenValidator(async function (context, event, callback) {
   const client = context.getTwilioClient();
   const response = new Twilio.Response();
   response.appendHeader('Access-Control-Allow-Origin', '*');
   response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST GET');
   response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
-  client.voice.v1.dialingPermissions
+  await client.voice.v1.dialingPermissions
       .countries
       .list({limit: 256})
       .then((countries) => {
-      response.appendHeader('Content-Type', 'application/json');
-      response.setBody({ content : countries });
-      return callback(null, response);
+        response.appendHeader('Content-Type', 'application/json');
+        response.setBody({ content : countries });
+        return callback(null, response);
       }
   ).catch((err) => {
       response.appendHeader('Content-Type', 'plain/text');
