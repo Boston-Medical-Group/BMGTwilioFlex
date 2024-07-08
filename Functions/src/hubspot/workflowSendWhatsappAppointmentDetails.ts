a44bc42e0131a9296b11581fce728ef673eed579
Jean Rumeau <rumeau@gmail.com>
2024-07-08 16:42:51 -0400

    corrige envío de detalle cita

--- Functions/src/hubspot/workflowSendWhatsappAppointmentDetails.ts
+++ Functions/src/hubspot/workflowSendWhatsappAppointmentDetails.ts
@@ -6,19 +6,16 @@ type MyContext = {
     ACCOUNT_SID: string
     AUTH_TOKEN: string
     TWILIO_WA_PHONE_NUMBER: string
+    HUBSPOT_TOKEN: string
     TASK_ROUTER_WORKSPACE_SID: string
     TASK_ROUTER_NOBODY_WORKFLOW_SID: string
-    HUBSPOT_TOKEN: string
 }
 
 type MyEvent = {
     request: any,
     cookies: any,
     template?: string
-    message?: string
-    flowSid: string
-    flowName?: string
-    contactId?: string
+    messagingService: string
     phone: string
     customParam?: string
     [key: string] : string | undefined
@@ -28,8 +25,6 @@ type MyEvent = {
  * 
  * Available parameters:
  * - template?
- * - message?
- * - flowSid
  * - contactId?
  * - phone
  * - objectId?
@@ -44,7 +39,7 @@ export const handler = async (
     event: MyEvent,
     callback: ServerlessCallback
 ) => {
-
+/*
     const authHeader = event.request.headers.authorization;
     const response = new Twilio.Response();
 
@@ -65,7 +60,7 @@ export const handler = async (
     // If the username or password don't match the expected values, reject
     if (username !== context.ACCOUNT_SID || password !== context.AUTH_TOKEN)
         return callback(null, setUnauthorized(response));
-
+*/
     const client = context.getTwilioClient()
 
     let parameters: Object = Object.keys(event)
@@ -77,14 +72,18 @@ export const handler = async (
             return newObj;
         }, {})
     
-    let attributes: { [key: string] : string } = Object.keys(event)
+    if (!event.hasOwnProperty('template')) {
+        return callback(null, 'ERROR: Missing template');
+    }
+
+    let attributes: { [key: string]: string } = Object.keys(event)
         .filter((k) => k.indexOf('param_') != 0 && k != 'request')
         .reduce((newObj, k) => {
             //@ts-ignore
             newObj[k] = event[k];
             return newObj;
         }, {})
-    
+
     const currentlyRequiredAttributes = ['customerName', 'name', 'crmid', 'hubspot_contact_id'];
     currentlyRequiredAttributes.forEach((k) => {
         if (!attributes.hasOwnProperty(k)) {
@@ -96,47 +95,37 @@ export const handler = async (
         }
     })
 
-    let returnObject : any = {'result': 'OK'};
+    let returnObject: any = { 'result': 'OK' };
 
     try {
         const whatsappAddressTo = event.phone.indexOf('whatsapp:') === -1 ? `whatsapp:${event.phone}` : `${event.phone}`
         const whatsappAddressFrom = context.TWILIO_WA_PHONE_NUMBER.indexOf('whatsapp:') === -1 ? `whatsapp:${context.TWILIO_WA_PHONE_NUMBER}` : `${context.TWILIO_WA_PHONE_NUMBER}`
-        const timestamp = (new Date).getTime();
-        await client.conversations.v1.conversations.create({
-            friendlyName: `HubspotWorkflow -> ${event.phone} (${timestamp})`,
-            attributes: JSON.stringify(attributes),
-            timers: {
-                inactive: 'PT1H',
-                closed: 'PT24H'
-            }
-        }).then(async (conversation) => {
-            return await client.conversations.v1.conversations(conversation.sid).participants.create({
-                //@ts-ignore
-                "messagingBinding.address": whatsappAddressTo,
-                "messagingBinding.proxyAddress": whatsappAddressFrom
-            }).then(async (participant) => {
-                return await client.conversations.v1.conversations(conversation.sid).webhooks.create({
-                    target: 'studio',
+        const activeConversation = await getActiveConversation(context, whatsappAddressTo)
+        if (activeConversation === null) {
+            const timestamp = (new Date).getTime();
+            await client.conversations.v1.conversations.create({
+                friendlyName: `HubspotWorkflow -> ${event.phone} (${timestamp})`,
+                attributes: JSON.stringify(attributes),
+                timers: {
+                    inactive: 'PT30M',
+                    closed: 'PT1H'
+                }
+            }).then(async (conversation) => {
+                return await client.conversations.v1.conversations(conversation.sid).participants.create({
                     //@ts-ignore
-                    "configuration.flowSid": event.flowSid
-                }).then(async (webhook) => {
+                    "messagingBinding.address": whatsappAddressTo,
+                    "messagingBinding.proxyAddress": whatsappAddressFrom
+                }).then(async (participant) => {
                     let msg;
-                    if (event.message) {
-                        msg = await client.conversations.v1.conversations(conversation.sid).messages.create({
-                            body: event.message
-                        })
-                    } else if (event.template) {
-                        msg = await client.conversations.v1.conversations(conversation.sid).messages.create({
-                            contentSid: event.template,
-                            contentVariables: JSON.stringify(parameters)
-                        })
-                    }
+                    msg = await client.conversations.v1.conversations(conversation.sid).messages.create({
+                        contentSid: event.template,
+                        contentVariables: JSON.stringify(parameters)
+                    })
 
                     await createNobodyTask({
                         context,
                         from: whatsappAddressTo,
                         conversationSid: conversation.sid,
-                        flowSid: event.flowSid,
                         flowName: event.flowName ?? 'Unknown Flow',
                         name: event.fullname,
                         leadOrPatient: event.leadOrPatient ?? '',
@@ -145,15 +134,63 @@ export const handler = async (
                         implementation: event.implementation ?? 'Transactional',
                         abandoned: event.abandoned ?? 'No',
                         customParam: event.customParam ?? ''
+                    }).then(async (task) => {
+                        await client.taskrouter.v1
+                            .workspaces(context.TASK_ROUTER_WORKSPACE_SID)
+                            .tasks
+                            .get(task.sid)
+                            .update({
+                                assignmentStatus: 'canceled'
+                            })
+                        
+                        await client.conversations.v1.conversations(conversation.sid).update({
+                            state: 'closed'
+                        })
                     })
+
 
                     returnObject = {
                         ...returnObject,
-                        ...msg
+                        sid: msg.sid,
+                        body: msg.body
                     }
                 })
             })
-        })
+        } else {
+            let msg;
+            msg = await client.conversations.v1.conversations(activeConversation).messages.create({
+                contentSid: event.template,
+                contentVariables: JSON.stringify(parameters)
+            })
+
+            await createNobodyTask({
+                context,
+                from: whatsappAddressTo,
+                conversationSid: activeConversation,
+                flowName: event.flowName ?? 'Unknown Flow',
+                name: event.fullname,
+                leadOrPatient: event.leadOrPatient ?? '',
+                contactId: event.contactId,
+                hubspotAccountId: event.hubspotAccountId ?? undefined,
+                implementation: event.implementation ?? 'Transactional',
+                abandoned: event.abandoned ?? 'No',
+                customParam: event.customParam ?? ''
+            }).then(async (task) => {
+                await client.taskrouter.v1
+                    .workspaces(context.TASK_ROUTER_WORKSPACE_SID)
+                    .tasks
+                    .get(task.sid)
+                    .update({
+                        assignmentStatus: 'canceled'
+                    })
+            })
+
+            returnObject = {
+                ...returnObject,
+                sid: msg.sid,
+                body: msg.body
+            }
+        }
     } catch (error) {
         console.log(error)
         returnObject.result = 'ERROR'
@@ -161,7 +198,21 @@ export const handler = async (
     }
 
     callback(null, returnObject)
+}
 
+const getActiveConversation = async (context: Context<MyContext>, whatsappAddressTo: string) => {
+    const client = context.getTwilioClient()
+    const conversations = await client.conversations.v1.participantConversations.list({
+        address: whatsappAddressTo,
+        limit: 50,
+    });
+
+    const activeConversation = conversations.find((conversation) => conversation.conversationState === 'active')
+    if (activeConversation != undefined) {
+        return activeConversation.conversationSid;
+    }
+
+    return null;
 }
 
 type ConversationsObject = {
@@ -172,7 +223,6 @@ type NobodyTaskParams = {
     context: Context<MyContext>
     from: string
     conversationSid: string
-    flowSid: string
     flowName?: string
     name?: string
     leadOrPatient?: string
@@ -191,7 +241,6 @@ const createNobodyTask = async ({
     context,
     from,
     conversationSid,
-    flowSid,
     flowName,
     name,
     leadOrPatient,
@@ -200,7 +249,7 @@ const createNobodyTask = async ({
     implementation,
     abandoned,
     customParam
-} : NobodyTaskParams) => {
+}: NobodyTaskParams) => {
     const client = context.getTwilioClient()
     const conversations: ConversationsObject = {}
     conversations.conversation_id = conversationSid;
@@ -212,8 +261,6 @@ const createNobodyTask = async ({
     conversations.communication_channel = "Chat";
     conversations.conversation_label_1 = "Conversation Sid";
     conversations.conversation_attribute_1 = conversationSid;
-    conversations.conversation_label_2 = "Flow Sid";
-    conversations.conversation_attribute_2 = flowSid;
     conversations.conversation_label_2 = "Flow Name";
     conversations.conversation_attribute_2 = flowName;
     conversations.conversation_label_4 = "BOT implementation";
@@ -237,8 +284,8 @@ const createNobodyTask = async ({
     }
 
     customers.customer_attribute_2 = `https://app-eu1.hubspot.com/contacts/${hubspotAccountId}/record/0-1/${contactId}`;
-    
-    await client.taskrouter.v1
+
+    return client.taskrouter.v1
         .workspaces(context.TASK_ROUTER_WORKSPACE_SID)
         .tasks.create({
             attributes: JSON.stringify({ "from": from, "name": name, conversations, customers }),
