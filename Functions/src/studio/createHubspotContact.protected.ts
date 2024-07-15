47339d68688d65753699f4fb8654e313a96f6933
Jean Rumeau <rumeau@gmail.com>
2024-07-15 08:58:05 -0400

    corrige busqueda de telefonos brasil

--- Functions/src/studio/createHubspotContact.protected.ts
+++ Functions/src/studio/createHubspotContact.protected.ts
@@ -52,6 +52,22 @@ exports.handler = async function (
   from = from.replace('whatsapp:', '');
   to = to.replace('whatsapp:', '');
 
+  // Fix para brasil
+  if (from.startsWith('+55')) {
+    let tmpPhone = from.replace('+55', '');
+    let prefix = tmpPhone.slice(0, 2);
+    let validPrefixes = ['11', '12', '13', '14', '15', '16', '17', '18', '19', '21', '22', '24', '27', '28', '31', '32', '33', '34',
+      '35', '37', '38', '41', '42', '43', '44', '45', '46', '47', '48', '49', '51', '53', '54', '55', '61', '62', '63', '64', '65',
+      '66', '67', '68', '69', '71', '73', '74', '75', '77', '79', '81', '82', '83', '84', '85', '86', '87', '88', '89', '91', '92',
+      '93', '94', '95', '96', '97', '98', '99'];
+    if (validPrefixes.includes(prefix)) {
+      let tmpPhoneWithoutPrefix = tmpPhone.slice(2);
+      if (tmpPhoneWithoutPrefix.length === 8) {
+        from = `+55${prefix}9${tmpPhoneWithoutPrefix}`
+      }
+    }
+  }
+
   const hubspotClient = new HubspotClient({ accessToken: context.HUBSPOT_TOKEN })
 
   await hubspotClient.crm.contacts.basicApi.create({
